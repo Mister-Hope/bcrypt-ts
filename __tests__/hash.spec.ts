d284493f8c3b90c638032e6249de2d840b17f718
Mister-Hope <mister-hope@outlook.com>
2024-12-08 16:27:09 +0800

    chore: bump deps

--- __tests__/hash.spec.ts
+++ __tests__/hash.spec.ts
@@ -4,10 +4,16 @@ import { hash, hashSync } from "../src/index.js";
 
 it("Hash sync", () => {
   expect(hashSync("hello", 10)).not.toEqual(hashSync("hello", 10));
+  expect(hashSync("中国我爱你！", 10)).not.toEqual(
+    hashSync("中国我爱你！", 10),
+  );
 });
 
 it("Hash async", () => {
   void hash("hello", 10).then((hash) => {
-    expect(typeof hash).toBe("string");
+    expect(hash).toBeTypeOf("string");
+  });
+  void hash("中国我爱你！", 10).then((hash) => {
+    expect(hash).toBeTypeOf("string");
   });
 });
--- __tests__/salt.spec.ts
+++ __tests__/salt.spec.ts
@@ -5,13 +5,13 @@ import { genSalt, genSaltSync } from "../src/index.js";
 it("Gen salt sync", () => {
   const salt = genSaltSync(10);
 
-  expect(typeof salt).toBe("string");
+  expect(salt).toBeTypeOf("string");
   expect(salt.length).toBe(29);
 });
 
 it("Gen salt async", async () => {
   const salt = await genSalt(10);
 
-  expect(typeof salt).toBe("string");
+  expect(salt).toBeTypeOf("string");
   expect(salt.length).toBe(29);
 });
