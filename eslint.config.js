80997e29f9d160d52a041f3f796f260dc25570b4
Mister-Hope <mister-hope@outlook.com>
2025-02-19 19:24:34 +0800

    chore: tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,9 @@
-import hopeConfig, { config, tsParser } from "eslint-config-mister-hope";
+import { hope } from "eslint-config-mister-hope";
 
-export default config(
-  ...hopeConfig,
-
-  {
-    ignores: ["**/dist/**", "**/node_modules/**", "coverage/**"],
-  },
-
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        parser: tsParser,
-        tsconfigDirName: import.meta.dirname,
-        project: ["./tsconfig.json"],
-      },
+export default hope({
+  ts: {
+    parserOptions: {
+      projectService: true,
     },
   },
-);
+});
