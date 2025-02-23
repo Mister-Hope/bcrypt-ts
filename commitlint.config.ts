bf780bdca35126a39d16d9b1973383a426d74fe5
Mister-Hope <mister-hope@outlook.com>
2025-02-23 12:31:39 +0800

    chore: tweaks

--- commitlint.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cz-git";
-
-export default defineConfig({
-  extends: ["@commitlint/config-conventional"],
-  prompt: {
-    allowCustomIssuePrefix: false,
-    allowEmptyIssuePrefix: false,
-  },
-});
