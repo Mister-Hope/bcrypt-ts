653afffc85bd8a27af27bd2f527f1b9145af6c34
Mister-Hope <mister-hope@outlook.com>
2025-06-07 13:45:18 +0800

    test: fix vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,17 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: {
-      random: path.resolve(__dirname, "src/random/node.ts"),
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      "@nextTick": path.resolve(__dirname, "src/nextTick/node.ts"),
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      "@random": path.resolve(__dirname, "src/random/node.ts"),
     },
   },
   test: {
     coverage: {
       provider: "istanbul",
       include: ["src/**/*.ts"],
+      exclude: ["src/**/browser.ts"],
     },
     include: ["**/*.spec.ts"],
 
