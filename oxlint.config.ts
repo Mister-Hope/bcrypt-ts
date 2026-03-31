99c1df2405d97ffac0aa13eb7f0a1649f337a691
Mr.Hope <mister-hope@outlook.com>
2026-03-31 10:43:35 +0800

    build: bump deps

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,20 +1,12 @@
-import { defaultIgnorePatterns, getOxlintConfigs } from "oxc-config-hope/oxlint";
-import { defineConfig } from "oxlint";
+import { defineHopeConfig } from "oxc-config-hope/oxlint";
 
-export default defineConfig({
-  extends: getOxlintConfigs({
-    vitest: {
-      bench: true,
-    },
-  }),
-  options: {
-    typeAware: true,
-    typeCheck: true,
-  },
-  ignorePatterns: defaultIgnorePatterns,
+export default defineHopeConfig({
   rules: {
     "id-length": "off",
     "no-bitwise": "off",
     "no-plusplus": "off",
+  },
+  vitest: {
+    bench: true,
   },
 });
